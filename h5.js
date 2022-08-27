126deee13fe27d5c287fe06602bb4143fa1340ea
Wy <githubWy@gmail.com>
2022-08-28 00:37:01 +0700

    add h5.js

--- h5.js
+++ h5.js
@@ -1,6 +1,5 @@
 let c = Number(prompt('e. Enter start number:'));
 let n = Number(prompt('e. Enter end number:'));
-for (let i=c; i<n; i+3)
-{
-    console.log(i);
+for( let i = c; i<n; i+=3){
+    console.log(i)
 }
