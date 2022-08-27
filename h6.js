b58bbeb36fa925b9e3273926a310cea3a7745f6f
Wy <githubWy@gmail.com>
2022-08-28 00:52:45 +0700

    add h6.js

---
+++ h6.js
@@ -0,0 +1,9 @@
+
+    var num = prompt("Nenter a number");
+    var factorialResult = 1;
+    for (var i = 1; i <= num; i++) {
+      factorialResult *= i;
+    }
+    console.log(factorialResult)
+
+  
