cfc16bf464663da24b571c914d1d005dba5a582f
Wy <githubWy@gmail.com>
2022-08-28 00:10:01 +0700

    add h4.js

--- h4.js
+++ h4.js
@@ -1,8 +1,3 @@
-// let n = prompt("enter  ending number n:")
-// for(var c = prompt("enter starting number c:"); c<n; c++){
-//     console.log(c)
-// }
-
 let c = prompt("enter starting number c:")
 let n = prompt("enter  ending number n:")
 while (c<=n){
